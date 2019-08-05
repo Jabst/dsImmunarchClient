30eb3299d21ae98d5d5257a3f38c5f4690a1ca32
José Alexandre Teixeira <jose.a.teixeira@inesctec.pt>
2019-08-05 16:32:30 +0100

    5th commit

--- R/ds.geneUsage.R
+++ R/ds.geneUsage.R
@@ -35,6 +35,8 @@
 #'   datashield.logout(opals)
 #'
 #' }
+#' 
+
 
 ds.geneUsage = function(x=NULL, type='combine', checks=FALSE, datasources=NULL){
   
@@ -42,42 +44,14 @@ ds.geneUsage = function(x=NULL, type='combine', checks=FALSE, datasources=NULL){
   # if no opal login details are provided look for 'opal' objects in the environment
   if(is.null(datasources)){
     datasources <- findLoginObjects()
+    
+    
   }
   
   if(is.null(x)){
     stop("Please provide the name of the input vector!", call.=FALSE)
   }
   
-  # the input variable might be given as column table (i.e. D$x)
-  # or just as a vector not attached to a table (i.e. x)
-  # we have to make sure the function deals with each case
-  xnames <- extract(x)
-  varname <- xnames$elements
-  obj2lookfor <- xnames$holders
-  #--------------------------------------------------------------------------------------------------#
-  #-------------------------------------- SERVER SIDE CHECKS ----------------------------------------#
-  if(checks){
-    # check if the input object(s) is(are) defined in all the studies
-    if(is.na(obj2lookfor)){
-      defined <- isDefined(datasources, varname)
-    }else{
-      defined <- isDefined(datasources, obj2lookfor)
-    }
-    
-    # call the internal function that checks the input object is of the same class in all studies.
-    typ <- checkClass(datasources, x)
-    
-    # the input object must be a numeric or an integer vector
-    if(typ != 'integer' & typ != 'numeric'){
-      stop("The input object must be an integer or a numeric vector.", call.=FALSE)
-    }
-  }
-  #----------------------------------------------------------------------------------------------------#
-  
-  # number of studies
-  num.sources <- length(datasources)
-  
-  #-------------------------------------- CALLING SERVER SIDE FUNCTION --------------------------------#
   
   cally <- paste0("geneUsageDS(", x, ", .gene=c('HomoSapiens.TRBJ')" ,")")
   # print(paste0("geneUsageDS(", x, ", .gene=c('HomoSapiens.TRBJ')" ,")"))
